f5f9d84a12b9de98ccca0a76e53007ff9513be16
tunaphish <tunaphishy37@gmail.com>
2021-12-06 15:59:51 -0800

    recenter buttons according to new dimensions

--- src/scenes/main-menu-scene.ts
+++ src/scenes/main-menu-scene.ts
@@ -15,16 +15,12 @@ export class MainMenuScene extends Phaser.Scene {
   }
 
   public create(): void {
-    this.add.text(0, 0, 'This is a sample main menu. Click the "Start" button below to run your game.', {
-      color: '#FFFFFF',
-    });
-
-    new MenuButton(this, 100, 150, 'Start Game', () => {
+    new MenuButton(this, 0, 150, 'Start Game', () => {
       this.scene.start('Game');
     });
 
-    new MenuButton(this, 100, 250, 'Settings', () => console.log('settings button clicked'));
+    new MenuButton(this, 0, 250, 'Settings', () => console.log('settings button clicked'));
 
-    new MenuButton(this, 100, 350, 'Help', () => console.log('help button clicked'));
+    new MenuButton(this, 0, 350, 'Help', () => console.log('help button clicked'));
   }
 }

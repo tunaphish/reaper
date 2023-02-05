c4cf7c9ba90c8d22b2399cdf3d5922e9da753de8
Tuan Pham <tunaphishy37@gmail.com>
2023-02-04 20:00:47 -0800

    add accelerometer parallax movement

--- src/scenes/battle/Battle.tsx
+++ src/scenes/battle/Battle.tsx
@@ -111,18 +111,41 @@ export class Battle extends Phaser.Scene {
     );
 
     this.ui.create(container, this);
-
+    
+    const _w = window.innerWidth / 2;
+    const _h = window.innerHeight / 2;
     this.parallax.addEventListener('mousemove', (e: MouseEvent) => {
-      const _w = window.innerWidth / 2;
-      const _h = window.innerHeight / 2;
       const _mouseX = e.clientX;
       const _mouseY = e.clientY;
       const _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`; // background
       const _depth2 = `${50 - (_mouseX - _w) * 0.03}% ${50 - (_mouseY - _h) * 0.03}%`; // portrait
       const _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`; // foreground
+
+      console.log(_mouseY)
+      console.log(_depth1);
+
       const x = `${_depth3}, ${_depth2}, ${_depth1}`;
       this.parallax.style.backgroundPosition = x;
     });
+
+    // parallax effect using accelerometer
+    if (window.DeviceMotionEvent !== undefined) {
+      console.log("accelerometer found");
+      window.addEventListener('deviceorientation', (e) => {
+        const gamma = Math.min(30, Math.max(-30, e.gamma));
+        const beta = Math.min(120, Math.max(60, e.beta));
+
+        const _depth1 = `${50 - (gamma - 0) * 0.1}% ${50 - (beta - 90) * 0.1}%`; // background
+        const _depth2 = `${50 - (gamma - 0) * 0.3}% ${50 - (beta - 90) * 0.3}%`; // portrait
+        const _depth3 = `${50 - (gamma - 0) * 0.6}% ${50 - (beta - 90) * 0.6}%`; // foreground
+        
+        const x = `${_depth3}, ${_depth2}, ${_depth1}`;
+    
+        this.parallax.style.backgroundPosition = x;
+      });
+
+    }
+
 
     this.actorDialogue.addEventListener('animationend', () => {
       this.isAnimatingText = false;

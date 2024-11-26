d5dbfbbd66b62ab382876e2f0748069794554733
Tuan Pham 🐟 <tunaphishy37@gmail.com>
2024-11-26 14:48:38 -0500

    install libraries, add checkered plane

--- src/scenes/r3f-test/R3FTest.tsx
+++ src/scenes/r3f-test/R3FTest.tsx
@@ -1,13 +1,40 @@
 import * as React from 'react';
 import ReactOverlay from '../../plugins/ReactOverlay';
-import { Canvas, advance } from "@react-three/fiber";
+import { Canvas, useLoader } from "@react-three/fiber";
 import { Physics, RigidBody } from "@react-three/rapier";
+import { OrbitControls } from '@react-three/drei';
+import { TextureLoader, RepeatWrapping } from 'three';
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   active: false,
   visible: false,
   key: 'R3FTest',
 };
+
+const Player = (props: { playerPosition: number[] }) => {
+  return (
+    <RigidBody position={props.playerPosition}>
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial  />
+      </mesh>
+    </RigidBody>
+  )
+}
+
+const Plane = () => {
+  const checkerTexture = useLoader(TextureLoader, '/reaper/assets/textures/checker.svg')
+  checkerTexture.wrapS = checkerTexture.wrapT = RepeatWrapping;
+  checkerTexture.repeat.set(10, 10); // Adjust the repeat for more tiles
+  return (
+    <RigidBody type="fixed" >
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]}>
+        <planeGeometry args={[100, 100]} /> 
+        <meshStandardMaterial map={checkerTexture} />
+      </mesh>
+    </RigidBody>
+  )
+}
 
 export class R3FTest extends Phaser.Scene {
   private reactOverlay: ReactOverlay;
@@ -22,26 +49,19 @@ export class R3FTest extends Phaser.Scene {
     }
 
     const R3F = () => {
+      const [playerPosition, setPlayerPosition] = React.useState<number[]>([0,10,0]);
       return (
         <>
           <Canvas>
+            <OrbitControls />
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[5, 10, 5]} castShadow />
             <Physics>
-              {/* Example Physics Objects */}
-              <RigidBody>
-                <mesh position={[0, 10, 0]}>
-                  <boxGeometry args={[1, 1, 1]} />
-                  <meshStandardMaterial  />
-                </mesh>
-              </RigidBody>
-              <RigidBody type="fixed">
-                <mesh position={[0, -1, 0]}>
-                  <boxGeometry args={[10, 1, 10]} />
-                  <meshStandardMaterial  />
-                </mesh>
-              </RigidBody>
+              <Player playerPosition={playerPosition}/>              
+              <Plane/>
             </Physics>
           </Canvas>
-          <div
+          {/* <div
             style={{
               position: "absolute",
               top: 0,
@@ -54,7 +74,7 @@ export class R3FTest extends Phaser.Scene {
             <div style={{ pointerEvents: "auto", color: 'black' }} onClick={clickhi}>
               hi
             </div>
-          </div>
+          </div> */}
         </>
 
       )

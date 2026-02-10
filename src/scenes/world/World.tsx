68327b6d530894879d1d960b1afe1d250cc799cc
Tuan Pham 🐟 <tunaphishy37@gmail.com>
2026-02-10 14:52:39 -0500

    add enter and exit triggers

--- src/scenes/world/World.tsx
+++ src/scenes/world/World.tsx
@@ -11,6 +11,9 @@ import { MenuState, WorldStore } from './worldStore';
 import { MapData } from '../../model/mapData';
 import { DEBUG_MAP_DATA } from '../../data/maps';
 
+import * as EXAMPLE_SPREADS from '../../data/spreads/example';
+import { Spread } from '../../model/spread';
+
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   active: false,
   visible: false,
@@ -22,6 +25,7 @@ export class World extends Phaser.Scene {
   reactOverlay: ReactOverlay;
   private music: Phaser.Sound.BaseSound;
   mapData: MapData;
+  triggerGroup!: Phaser.Physics.Arcade.StaticGroup
 
   worldStore: WorldStore;
 
@@ -60,10 +64,26 @@ export class World extends Phaser.Scene {
 
     const spawnPoint = map.findObject('Objects', (obj) => obj.name === 'Spawn Point');
 
+    // Create Map Triggers
+    this.triggerGroup = this.physics.add.staticGroup()
+    // TODO: Update to pull trigger data from Tiled
+    this.createEncounterTriggers(spawnPoint)
+
+
     // Player
     this.player = new Player(this, spawnPoint.x, spawnPoint.y);
     this.physics.add.collider(this.player, worldLayer);
     this.cameras.main.startFollow(this.player);
+
+    // Map Triggers
+    this.physics.add.overlap(
+      this.player,
+      this.triggerGroup,
+      this.onTriggerOverlap,
+      undefined,
+      this
+    )
+
 
     this.cameras.main.fadeIn(1200);
     if (this.mapData.musicKey) {
@@ -79,6 +99,14 @@ export class World extends Phaser.Scene {
 
   update(time: number, delta: number): void {
     this.player.update(time, delta);
+    this.triggerGroup.children.iterate(zone => {
+      const overlapping = zone.getData("overlapping");
+      if (overlapping && !this.physics.overlap(zone, this.player)) {
+        const encounter: Spread = zone.getData("encounter");
+        zone.setData("overlapping", false);
+        console.log('exit: ', encounter.id);
+      }
+    });
   }
 
   pause(): void {
@@ -112,5 +140,48 @@ export class World extends Phaser.Scene {
 
   playChoiceDisabledSound(): void {
     this.choiceDisabledSound.play();
+  }
+
+  createEncounterTriggers(spawnPoint: Phaser.Types.Tilemaps.TiledObject): void {
+    const triggers = [
+      {
+        triggerId: 'example_trigger_id',
+        encounter: EXAMPLE_SPREADS.BUNNY_MASK_SPREAD,
+        x: spawnPoint.x,
+        y: spawnPoint.y,
+        width: 48,
+        height: 48
+      }
+    ]
+
+    for (const data of triggers) {
+      const zone = this.add.zone(
+        data.x,
+        data.y,
+        data.width,
+        data.height
+      );
+
+      this.physics.add.existing(zone, true);
+
+      zone.setData("encounter", data.encounter);
+      zone.setData("triggerId", data.triggerId);
+      zone.setData("overlapping", false);
+
+      this.triggerGroup.add(zone);
+    }
+}
+
+  onTriggerOverlap(
+    player: Phaser.GameObjects.GameObject,
+    zone: Phaser.GameObjects.GameObject
+  ): void {
+    const overlapping = zone.getData("overlapping");
+    if (overlapping) return;
+    zone.setData("overlapping", true);
+
+    // play out encounter
+    const encounter: Spread  = zone.getData("encounter");
+    console.log('enter: ', encounter.id);
   }
 }

629aa69d509ec1c5ce64d0f6ffeb328aca3e020f
Tuan Pham 🐟 <tunaphishy37@gmail.com>
2026-02-24 13:06:12 -0500

    remove encounter and encounter list scenes

--- src/scenes/index.ts
+++ src/scenes/index.ts
@@ -1,9 +1,7 @@
 import { MainMenu } from './main-menu/MainMenu';
 import { Boot } from './boot/Boot';
 import { World } from './world/World';
-import { Encounter } from './encounter/Encounter';
 import { Battle } from './battle/Battle';
-import { EncounterList } from './encounter-list/EncounterList';
 import { GameOver } from './game-over/GameOver'
 
-export default [Boot, MainMenu, World, EncounterList, Encounter, Battle, GameOver];  
+export default [Boot, MainMenu, World, Battle, GameOver];  
